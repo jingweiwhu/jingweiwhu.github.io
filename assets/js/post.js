70d5b4eb5f3ad7c5174296db9dd9bcfc2de7fafc
Jing Wei <jingweiwhu@126.com>
2016-12-09 12:17:11 +0800

    update

--- assets/js/post.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $("pre").addClass("prettyprint");
-  prettyPrint();
-  $("pre").addClass("pre-scrollable");
-    $("pre").addClass("language-basic");
-  $("img").addClass("center-block img-responsive img-thumbnail");
-  $("table").addClass("table table-striped table-bordered table-hover table-condensed");
-  $("p a").addClass("text-info");
-});
